3e1de0ffd0407488f6e57e64b1a4a79728bcdd9a
Sunny Garg <sunny.shiva@gmail.com>
2016-06-30 22:36:15 +0530

    modiefied carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,6 +4,11 @@ CarrierWave.configure do |config|
     provider:              'AWS',                        # required
     aws_access_key_id:     ENV['aws_access_key_id'],                        # required
     aws_secret_access_key: ENV['aws_secret_access_key'],                        # required
+    :region                 => 'eu-west-1',                  # optional, defaults to 'us-east-1'
+    :host                   => 's3.example.com',             # optional, defaults to nil
+    :endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
   }
   config.fog_directory  = ENV['fog_directory']                          # required
+  config.fog_public     = false                                   # optional, defaults to true
+  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
